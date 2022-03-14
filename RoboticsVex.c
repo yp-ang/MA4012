698744b1222a6ba09c3ced9630d9414c2d2caf97
Tan Cher Yang <tan.cheryang@gmail.com>
2022-03-14 12:40:50 +0800

    Add definitions for ports, distance, Add some functions

--- RoboticsVex.c
+++ RoboticsVex.c
@@ -1,10 +1,36 @@
 #pragma config(Sensor, dgtl7, Floor_frontLeft, sensorDigitalIn)
 #pragma config(Sensor, dgtl8, Floor_frontRight, sensorDigitalIn)
 
-#define left_motor port3
-#define right_motor port2
-#define speedOffset 0
 
+//Motor Definitions
+
+#define LEFT_MOTOR port3
+#define RIGHT_MOTOR port2
+#define SPEED_OFFSET 0.0
+#define SLOW_SPEED 20.0
+//Limit Switch Definitions
+
+#define DOOR_LSWITCH_PORT dgtl2
+#define BALL_LSWITCH_PORT dgtl1
+//Distance IR Sensor Definitions
+
+#define LEFT_DISTANCE_IR_TYPE ORANGE_YELLOW
+#define LEFT_DISTANCE_IR_PORT in1
+#define RIGHT_DISTANCE_IR_TYPE ORANGE_GREEN
+#define RIGHT_DISTANCE_IR_PORT in2
+#define DOOR_DISTANCE_IR_TYPE ORANGE_BLUE
+#define DOOR_DISTANCE_IR_PORT in3
+
+//Reflective Sensor Definitions
+#define LEFT_REFLECTIVE_IR_PORT in4
+#define RIGHT_REFLECTIVE_IR_PORT in5
+
+//Utils
+#define WIFI_DEBUGGING 0
+#define UART_PORT uartOne
+
+
+enum ir_sensor{ORANGE_PINK, ORANGE_GREEN, ORANGE_YELLOW, ORANGE_BLUE};
 enum direction{STOP, REVERSE, STRAIGHT, CLOCKWISE, CCLOCKWISE};
 enum bearing{NORTH, NORTHEAST, EAST, SOUTHEAST, SOUTH, SOUTHWEST, WEST, NORTHWEST, ERROR};
 
@@ -15,12 +41,38 @@ YuPin edit 22/2/2022
 
 */
 
-
-//port2 motor goes to 9/10
-//port3 motor goes to 11/12
 const int slow_speed = 20;
-//const int speedOffset = 5;
 
+void send_debug_msg(char* msg, int size)
+{
+#ifdef WIFI_DEBUGGING
+	for (int i = 0; i < size; i ++)
+	{
+		sendChar(UART_PORT, msg[i]);
+	}
+#endif
+}
+
+float convert_ir_reading_to_distance(ir_sensor sensor, int analog)
+{
+	switch(sensor)
+	{
+	case ORANGE_BLUE:
+		return 2316.4 * pow(analog, -0.998);
+		break;
+	case ORANGE_GREEN:
+		return 49865 * pow(analog, -1.416);
+		break;
+	case ORANGE_PINK:
+		return 166922 * pow(analog, -1.604);
+		break;
+	case ORANGE_YELLOW:
+		return 44924 * pow(analog, -1.394);
+		break;
+	default:
+		return -1;
+	}
+}
 
 void start_process(){
 	//movement to middle of field
@@ -30,28 +82,28 @@ void movement(direction directionMode, int speed){
 	switch (directionMode)
 	{
 	case STOP:
-		motor[left_motor] = 0;
-		motor[right_motor] = 0;
+		motor[LEFT_MOTOR] = 0;
+		motor[RIGHT_MOTOR] = 0;
 		break;
 	case STRAIGHT:
-		motor[left_motor] = -speed;
-		motor[right_motor] = speed;
+		motor[LEFT_MOTOR] = -speed;
+		motor[RIGHT_MOTOR] = speed;
 		break;
 	case REVERSE:
-		motor[left_motor] = speed;
-		motor[right_motor] = -speed;
+		motor[LEFT_MOTOR] = speed;
+		motor[RIGHT_MOTOR] = -speed;
 		break;
 	case CLOCKWISE:
-		motor[left_motor] = speed;
-		motor[right_motor] = speed;
+		motor[LEFT_MOTOR] = speed;
+		motor[RIGHT_MOTOR] = speed;
 		break;
 	case CCLOCKWISE:
-		motor[left_motor] = -speed;
-		motor[right_motor] = -speed;
+		motor[LEFT_MOTOR] = -speed;
+		motor[RIGHT_MOTOR] = -speed;
 		break;
 	default:
-		motor[left_motor] = 0;
-		motor[right_motor] = 0;
+		motor[LEFT_MOTOR] = 0;
+		motor[RIGHT_MOTOR] = 0;
 		break;
 	}
 }
@@ -92,6 +144,7 @@ void move_to_ball(/*distance*/){
 }
 
 bool detect_ball_collector(){
+	return (SensorValue(BALL_LSWITCH_PORT) == 1);
 }
 
 void align_orientation(){
@@ -107,6 +160,9 @@ void move_to_collection(){
 //////////////////////////////////////////////////////////
 task main()
 {
+#ifdef WIFI_DEBUGGING
+	setBaudRate(UART_PORT, baudRate9600);
+#endif
 	start_process();
 	while (true)
 	{
